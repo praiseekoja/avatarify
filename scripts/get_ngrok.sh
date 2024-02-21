190eda0b1e3ec731dd45f2cb7da1acd56e5e95e1
Ekoja cheto <108088240+praiseekoja@users.noreply.github.com>
2024-02-21 14:33:37 +0100

    Update get_ngrok.sh

--- scripts/get_ngrok.sh
+++ scripts/get_ngrok.sh
@@ -4,7 +4,7 @@ command -v ./ngrok >/dev/null 2>&1
 if [[ $? -ne 0 ]]; then
     echo ngrok is not found, installing...
     wget  https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-amd64.tgz
-    sudo tar xvzf ./ngrok-v3-stable-linux-amd64.tgz -C /usr/local/bin
+    sudo tar xvzf ./ngrok-v3-stable-linux-amd64.tgz 
     echo Done!
 fi
 
